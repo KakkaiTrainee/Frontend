48279723f7b6a1d924f4aca5d514ffaa6a817e83
chanathipgeist <chanathip.l5139@kmutt.ac.th>
2023-07-04 23:12:40 +0700

    design architech

---
+++ src/composables/fetch.js
@@ -0,0 +1,11 @@
+export default getRegions = async () => {
+    try{
+        //const res = await fetch(`${import.meta.env.VITE_BASE_URL}/api/regions`)
+        const res = await fetch(`http://localhost:8081/api/regions`)
+        if(res.ok){
+            return await res.json()
+        }else throw new Error(`Error,cannot get Region data`)
+    }catch(error){
+        console.log(error)
+    }
+}
